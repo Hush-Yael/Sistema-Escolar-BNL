4a343c01da758526e8b7a0a7008abcc0ff7430f6
Jean <srjean21@gmail.com>
2026-06-03 12:03:32 -0400

    feat: app theme mode is in sync with state

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:sistema_escolar_bnl/core/auth_state.dart';
 import 'package:sistema_escolar_bnl/core/navigation/router.dart';
 import 'package:sistema_escolar_bnl/core/setup_page.dart';
 import 'package:sistema_escolar_bnl/core/theme/theme.dart';
+import 'package:sistema_escolar_bnl/core/theme/theme_mode.dart';
+import 'package:flutter_solidart/flutter_solidart.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,22 +19,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ShadApp.custom(
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      appBuilder: (context) {
-        return MaterialApp.router(
-          theme: Theme.of(context),
-          debugShowCheckedModeBanner: false,
-          localizationsDelegates: const [
-            GlobalShadLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-          ],
-          routerConfig: AppRouter(
-            authState: AuthState.instance.of(context),
-          ).config,
-          builder: (context, child) => ShadAppBuilder(child: child),
+    final themeMode = ThemeModeState.instance.of(context);
+
+    return SignalBuilder(
+      builder: (context, child) {
+        return ShadApp.custom(
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: themeMode.state.value,
+          appBuilder: (context) {
+            return MaterialApp.router(
+              theme: Theme.of(context),
+              debugShowCheckedModeBanner: false,
+              localizationsDelegates: const [
+                GlobalShadLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+              ],
+              routerConfig: AppRouter(
+                authState: AuthState.instance.of(context),
+              ).config,
+              builder: (context, child) => ShadAppBuilder(child: child),
+            );
+          },
         );
       },
     );
