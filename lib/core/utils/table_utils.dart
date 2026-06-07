65f0973f611cff99008fcf2c8eb6a6bb26675daf
Jean <srjean21@gmail.com>
2026-06-07 12:15:17 -0400

    feat: now "$id" getter provides better error cause when it can't find the id

--- lib/core/utils/table_utils.dart
+++ lib/core/utils/table_utils.dart
@@ -2,7 +2,17 @@ import 'package:trina_grid/trina_grid.dart';
 
 extension TrinaRowExt on TrinaRow {
   /// returns the id (if provided) of the object associated with the row
-  int get $id => metadata!['id'];
+  int get $id {
+    if (metadata == null) {
+      throw Exception('Row metadata was not defined');
+    }
+
+    if (!metadata!.containsKey('id')) {
+      throw Exception('Row metadata does not contain "id" key');
+    }
+
+    return metadata!['id'];
+  }
 
   set $id(int id) => metadata!['id'] = id;
 
