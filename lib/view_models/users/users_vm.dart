e403377efa2fd799dd0c93c549e77928304dd7b0
Jean <srjean21@gmail.com>
2026-06-09 17:40:56 -0400

    refactor: merged users view model mutations mixin with main class

--- lib/view_models/users/users_vm.dart
+++ lib/view_models/users/users_vm.dart
@@ -1,9 +1,14 @@
 import 'package:disco/disco.dart';
+import 'package:flutter/widgets.dart';
 import 'package:sistema_escolar_bnl/core/auth_state.dart';
 import 'package:sistema_escolar_bnl/core/db/db.dart';
 import 'package:sistema_escolar_bnl/repositories/users_repo.dart';
 import 'package:sistema_escolar_bnl/shared/table/table_vm.dart';
-import 'package:sistema_escolar_bnl/view_models/users/mutations_mixin.dart';
+import 'package:sistema_escolar_bnl/constants/auth_constants.dart';
+import 'package:sistema_escolar_bnl/constants/users_constants.dart';
+import 'package:sistema_escolar_bnl/shared/mutations/index.dart';
+import 'package:sistema_escolar_bnl/shared/mutations/single_delete.dart';
+import 'package:sistema_escolar_bnl/shared/mutations/single_update.dart';
 
 class UsersVm extends TableVm {
   @override
@@ -11,16 +16,36 @@ class UsersVm extends TableVm {
 
   final AuthState authState = .instance;
 
-  UsersBaseVm({required this.repository});
-}
-
-class UsersVm extends UsersBaseVm with MutationsMixin {
-  UsersVm({required super.repository});
+  UsersVm(this.repository);
 
   static final instance = Provider((ctx) {
     final AppDatabase db = AppDatabase.instance.of(ctx);
     final usersRepository = UsersRepository(db);
 
-    return UsersVm(repository: usersRepository);
+    return UsersVm(usersRepository);
   });
+
+  MutationCommonParams<S> _params<S extends Function>(BuildContext context) =>
+      MutationCommonParams<S>(
+        context,
+        timestamped: true,
+        queryKey: kUsersQueryKey,
+        getStateManager: getStateManager,
+        successName: 'usuario',
+        unauthPluralName: 'usuarios',
+        successMsgVocal: 'o',
+      );
+
+  SingleDeleteMutation createDeleteMutation(BuildContext context) =>
+      createSingleDeleteMutation(
+        .fromCommonParams(_params(context), repository.deleteUser),
+      );
+
+  SingleUpdateMutation createUpdateMutation(BuildContext context) =>
+      createSingleUpdateMutation(
+        _params(context),
+        cb: (column, id, value) => repository.changeRole(id, value),
+        getValue: (event, ctx) =>
+            UserRole.values.firstWhere((role) => role.label == event.value),
+      );
 }
