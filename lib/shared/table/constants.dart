603c6b933766b145032bb030bb352640a946c802
Jean <srjean21@gmail.com>
2026-06-12 18:51:10 -0400

    refactor: adapted index and actions columns to be in a ColumnsEnum

--- lib/shared/table/constants.dart
+++ lib/shared/table/constants.dart
@@ -1,10 +1,18 @@
+import 'package:sistema_escolar_bnl/shared/table/columns.dart';
 import 'package:trina_grid/trina_grid.dart';
 
-/// Used to identify the column that contains the index of the row
-const kIndexColumnField = 'index';
+/// [Index] Used to identify the column that contains the index of the row
+/// [actions] Used to identify the column that contains the actions of the row
+enum TableSpecialCols implements ColumnsEnum {
+  // ignore: constant_identifier_names
+  Index('#'),
+  actions('Acciones');
 
-/// Used to identify the column that contains the actions of the row
-const kActionsColumnName = 'actions';
+  const TableSpecialCols(this.title);
+
+  @override
+  final String title;
+}
 
 final formattedDateColumnType = TrinaColumnType.date(
   format: 'EEE, dd MMM yyyy hh:mm a',
